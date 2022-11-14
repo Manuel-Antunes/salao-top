b11b084bdc0ba5cb81a8f1bbcfdb62ddfa1f3705
Manuel-Antunes <nerdbroz2@gmail.com>
2022-11-14 11:02:12 -0300

    primeiro commit

---
+++ src/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBYwt-QbOrfACF12zCRK__BTLStOffdplg",
+  authDomain: "salao-top.firebaseapp.com",
+  projectId: "salao-top",
+  storageBucket: "salao-top.appspot.com",
+  messagingSenderId: "585122529922",
+  appId: "1:585122529922:web:da58cb39df7443a34c2a9b",
+  measurementId: "G-NF35VEM7QD"
+};
+
+module.exports = firebaseConfig;
